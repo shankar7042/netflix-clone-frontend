faf9ff3afb73f6e0e22adbce11ebd932c1827139
shankar7042 <shankar.kumar.4171@gmail.com>
2020-09-27 12:14:00 +0530

    Add footer component

---
+++ src/containers/footer.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Footer } from "../components";
+
+export function FooterContainer() {
+  return (
+    <Footer>
+      <Footer.Title>Question? Contact us</Footer.Title>
+      <Footer.Break />
+      <Footer.Row>
+        <Footer.Column>
+          <Footer.Link href="#">FAQ</Footer.Link>
+          <Footer.Link href="#">Investor Relations</Footer.Link>
+          <Footer.Link href="#">Ways to watch</Footer.Link>
+          <Footer.Link href="#">Corporate Information</Footer.Link>
+          <Footer.Link href="#">Netflix Originals</Footer.Link>
+        </Footer.Column>
+        <Footer.Column>
+          <Footer.Link href="#">FAQ</Footer.Link>
+          <Footer.Link href="#">Investor Relations</Footer.Link>
+          <Footer.Link href="#">Ways to watch</Footer.Link>
+          <Footer.Link href="#">Corporate Information</Footer.Link>
+          <Footer.Link href="#">Netflix Originals</Footer.Link>
+        </Footer.Column>
+        <Footer.Column>
+          <Footer.Link href="#">FAQ</Footer.Link>
+          <Footer.Link href="#">Investor Relations</Footer.Link>
+          <Footer.Link href="#">Ways to watch</Footer.Link>
+          <Footer.Link href="#">Corporate Information</Footer.Link>
+          <Footer.Link href="#">Netflix Originals</Footer.Link>
+        </Footer.Column>
+        <Footer.Column>
+          <Footer.Link href="#">FAQ</Footer.Link>
+          <Footer.Link href="#">Investor Relations</Footer.Link>
+          <Footer.Link href="#">Ways to watch</Footer.Link>
+          <Footer.Link href="#">Corporate Information</Footer.Link>
+          <Footer.Link href="#">Netflix Originals</Footer.Link>
+        </Footer.Column>
+      </Footer.Row>
+      <Footer.Break />
+      <Footer.Text>Netflix India</Footer.Text>
+    </Footer>
+  );
+}
