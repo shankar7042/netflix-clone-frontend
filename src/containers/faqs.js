807666639bcfaab6fd696696fc721ccce3922678
shankar7042 <shankar.kumar.4171@gmail.com>
2020-09-27 12:17:31 +0530

    Add Faqs container to show to the user

---
+++ src/containers/faqs.js
@@ -0,0 +1,26 @@
+import React from "react";
+import faqsData from "../fixtures/faqs.json";
+import { Accordion, OptForm } from "../components";
+
+export function FaqsContainer() {
+  return (
+    <Accordion>
+      <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+      {faqsData.map((item) => (
+        <Accordion.Item key={item.id}>
+          <Accordion.Header>{item.header}</Accordion.Header>
+          <Accordion.Body>{item.body}</Accordion.Body>
+        </Accordion.Item>
+      ))}
+      <OptForm>
+        <OptForm.Text>
+          Ready to watch? Enter your email to create or restart your membership.
+        </OptForm.Text>
+        <OptForm.Container>
+          <OptForm.Input placeholder="Email Address" />
+          <OptForm.Button>Get Started</OptForm.Button>
+        </OptForm.Container>
+      </OptForm>
+    </Accordion>
+  );
+}
