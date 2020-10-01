ee5b626319b7eaf5c99923c08e255c46962de0ea
shankar7042 <shankar.kumar.4171@gmail.com>
2020-10-01 16:42:31 +0530

    made a component named profiles

---
+++ src/components/profiles/styles/profiles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components/macro";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const Title = styled.h1``;
+export const List = styled.ul``;
+export const User = styled.li``;
+export const Picture = styled.img``;
+export const Name = styled.p``;
